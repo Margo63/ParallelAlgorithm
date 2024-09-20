55854646783dd4652fadbaab33b420207b2836ed
Margo63 <margo-vinograd@mail.ru>
2024-09-20 21:20:55 +0300

    add multiplication

--- main.cpp
+++ main.cpp
@@ -7,39 +7,6 @@
 #include <string>
 #include <vector>
 
-//int main(){
-//    pid_t pid;
-//    int ret;
-//    int a=0;
-//
-//    pid = fork();
-//
-//    a+=5;
-//
-//    //pid_t pid1 = fork();
-//
-//    switch (pid){
-//        // весь код после fork() дважды:
-//        // в родительском процессе и в процессе потомке
-//
-//        case -1: //неудачная попытка создать процесс-потомок
-//            exit(-1); //выход из родительского процесса
-//
-//        case 0: //потомок
-//            std::cout<<"CHILD:: Hello, my pid = "<< getpid()<<"; my parent's pid (ppid) = "<<getppid()<<std::endl;
-//            a+=10;
-//            std::cout<<a<<std::endl;
-//            exit(ret);
-//
-//        default: //родитель pid>0
-//            std::cout<<"PARENT:: Hello, my pid = "<< getpid()<<"; my child's pid = "<<pid<<std::endl;
-//            a+=20;
-//            std::cout<<a<<std::endl;
-//            wait(&pid);
-//    }
-//
-//    return 0;
-//}
 std::vector<int> split_in_num(std::string line) {
     char *copy = const_cast<char *>(line.c_str());
     char *token = std::strtok(copy, " ");
@@ -51,7 +18,7 @@ std::vector<int> split_in_num(std::string line) {
     return result;
 }
 
-void read_matrix(std::string name_file, std::vector<std::vector<int>>* matrix) {
+void read_matrix(std::string name_file, std::vector<std::vector<int>> *matrix) {
     std::ifstream file;
     file.open(name_file);
 
@@ -74,33 +41,13 @@ void read_matrix(std::string name_file, std::vector<std::vector<int>>* matrix) {
     while (getline(file, line)) {
         matrix->push_back(split_in_num(line));
     }
+    std::cout<<"finish read\n";
 
     file.close();
 }
 
-int main() {
-
-    std::vector<std::vector<int>> matrixA;
-
-    read_matrix("../input.txt", &matrixA);
-//    for (int i = 0; i < matrixA.size(); i++) {
-//        for (int j = 0; j < matrixA[i].size(); j++) {
-//            std::cout<<matrixA[i][j]<<" ";
-//        }
-//        std::cout<<"\n";
-//    }
-
-    std::vector<std::vector<int>> matrixB;
-
-    read_matrix("../test.txt", &matrixB);
-//    for (int i = 0; i < matrixB.size(); i++) {
-//        for (int j = 0; j < matrixB[i].size(); j++) {
-//            std::cout<<matrixB[i][j]<<" ";
-//        }
-//        std::cout<<"\n";
-//    }
-
-    std::vector<std::vector<int>> matrixC;
+void multiplication(std::vector<std::vector<int>> matrixA, std::vector<std::vector<int>> matrixB,
+                    std::vector<std::vector<int>> *matrixC) {
     for (int i = 0; i < matrixA.size(); i++) {
         std::vector<int> line;
         for (int j = 0; j < matrixB[0].size(); j++) {
@@ -108,37 +55,61 @@ int main() {
             for (int k = 0; k < matrixB.size(); k++) {
                 cur += matrixA[i][k] * matrixB[k][j];
             }
-            //line.push_back(cur);
-            std::cout << cur << "\t";
+            line.push_back(cur);
+            //std::cout << cur << "\t";
         }
-        //matrixC.push_back(line);
+        matrixC->push_back(line);
 
+        //std::cout << "\n";
+    }
+}
+
+void print(std::vector<std::vector<int>> matrix) {
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            std::cout << matrix[i][j] << " ";
+        }
         std::cout << "\n";
     }
 
-//    pid_t pid_set_info = fork();
-//    pid_t pid_multiplication = fork();
-//    pid_t pid_print_result = fork();
+}
+
+int main() {
+    std::vector<std::vector<int>> matrixA, matrixB,matrixC;
+
+    pid_t pid = fork();
+
+    switch (pid){
+        // весь код после fork() дважды:
+        // в родительском процессе и в процессе потомке
+
+        case -1: //неудачная попытка создать процесс-потомок
+            exit(-1); //выход из родительского процесса
+
+        case 0: //потомок
+            std::cout<<"CHILD:: Hello, my pid = "<< getpid()<<"; my parent's pid (ppid) = "<<getppid()<<std::endl;
+            multiplication(matrixA, matrixB, &matrixC);
+            print(matrixC);
+
+            return 0;
+
+        default: //родитель pid>0
+            std::cout<<"PARENT:: Hello, my pid = "<< getpid()<<"; my child's pid = "<<pid<<std::endl;
+            read_matrix("../input.txt", &matrixA);
+            read_matrix("../test.txt", &matrixB);
+            wait(&pid);
+    }
+
+
+
+
+//    read_matrix("../input.txt", &matrixA);
+//    read_matrix("../test.txt", &matrixB);
 //
-//    switch (pid_set_info) {
-//        // весь код после fork() дважды:
-//        // в родительском процессе и в процессе потомке
+//    multiplication(matrixA, matrixB, &matrixC);
+//    print(matrixC);
 //
-//        case -1: //неудачная попытка создать процесс-потомок
-//            exit(-1); //выход из родительского процесса
-//
-//        case 0: //потомок
-//            if()
-//            std::cout << "CHILD:: Hello, my pid = " << getpid() << "; my parent's pid (ppid) = " << getppid()
-//                      << std::endl;
-//
-//            exit(0);
-//
-//        default: //родитель pid>0
-//            std::cout << "PARENT:: Hello, my pid = " << getpid() << "; my child's pid = " << pid_set_info << std::endl;
-//
-//            wait(&pid_set_info);
-//    }
+
 
 
     return 0;
